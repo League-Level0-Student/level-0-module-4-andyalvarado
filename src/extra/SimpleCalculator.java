d42b3208ed224879c83eab1f755d986c2c13761e
ilabstaff <ilabstaff@172.16.116.77>
2019-07-03 17:47:27 -0700

    simplecalculater

--- src/extra/SimpleCalculator.java
+++ src/extra/SimpleCalculator.java
@@ -7,12 +7,13 @@ public class SimpleCalculator {
 	public static void main(String[] args) {
 
 		// 1. Get 2 numbers from the user and convert them to integer.
-String rb =JOptionPane.showInputDialog("choose 1 number");
-String ra =JOptionPane.showInputDialog("choose another number");
+String rb =JOptionPane.showInputDialog("Insert number");
+
+String ra =JOptionPane.showInputDialog("Insert another number");
 int cv = Integer.parseInt(rb);
 int fh = Integer.parseInt(ra);
 // 2. Customize pop-up to support add/subtract/multiply/divide operations.
-		int operation = JOptionPane.showOptionDialog(null, "insert math problem", " add/subtract/multiply/divide", 0,
+		int operation = JOptionPane.showOptionDialog(null, "choose a mathematical problem", " calculator", 0,
 				JOptionPane.INFORMATION_MESSAGE, null, new String[] { "add", "subtract", "multiply", "divide" },
 				null);
 
